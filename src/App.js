ef08bdf9e191f6eb5ca16dd9b4dad08fcef8d7e1
SDioni <stevendioni@gmail.com>
2020-12-02 22:58:55 -0600

    add initial react template

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+
+
+        <ul class="set">
+          <li class="white b"></li>
+          <li class="black as"></li>
+          <li class="white a"></li>
+          <li class="black gs"></li>
+          <li class="white g"></li>
+          <li class="black fs"></li>
+          <li class="white f"></li>
+          <li class="white e"></li>
+          <li class="black ds"></li>
+          <li class="white d"></li>
+          <li class="black cs"></li>
+          <li class="white c"></li>
+          <li class="white b"></li>
+          <li class="black as"></li>
+          <li class="white a"></li>
+          <li class="black gs"></li>
+          <li class="white g"></li>
+          <li class="black fs"></li>
+          <li class="white f"></li>
+          <li class="white e"></li>
+          <li class="black ds"></li>
+          <li class="white d"></li>
+          <li class="black cs"></li>
+          <li class="white c"></li>
+        </ul>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
